09391ac10bb9579e52d406215f142646e8a315cb
Vigile Martinier <virgile.martinier6002@gmail.com>
2025-10-31 00:20:18 +0100

    Etape 1

---
+++ src/Entity/Comment.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommentRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CommentRepository::class)]
+class Comment
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Name = null;
+
+    #[ORM\Column]
+    private ?int $Value = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $Comment = null;
+
+    #[ORM\Column(type: Types::DATE_IMMUTABLE)]
+    private ?\DateTimeImmutable $CreationDate = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->Name;
+    }
+
+    public function setName(string $Name): static
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+
+    public function getValue(): ?int
+    {
+        return $this->Value;
+    }
+
+    public function setValue(int $Value): static
+    {
+        $this->Value = $Value;
+
+        return $this;
+    }
+
+    public function getComment(): ?string
+    {
+        return $this->Comment;
+    }
+
+    public function setComment(string $Comment): static
+    {
+        $this->Comment = $Comment;
+
+        return $this;
+    }
+
+    public function getCreationDate(): ?\DateTimeImmutable
+    {
+        return $this->CreationDate;
+    }
+
+    public function setCreationDate(\DateTimeImmutable $CreationDate): static
+    {
+        $this->CreationDate = $CreationDate;
+
+        return $this;
+    }
+}
